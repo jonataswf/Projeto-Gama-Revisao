5bbfcc89d347fbfc4cc16c2942e7efe08c4a35e4
Jonatas <jonatas.wf@gmail.com>
2022-03-19 17:33:33 -0300

    inclusao JOptionPane

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Sistema {
@@ -17,7 +18,6 @@ public class Sistema {
         File arq = new File(caminhoArquivo);
         arq.delete();
 
-
         while (menu) {
 
             System.out.println("-------Sistema Banco-------");
@@ -29,90 +29,92 @@ public class Sistema {
             System.out.println("---------------------------");
             System.out.print("Digite a opção: ");
 
-            opcao = sc.nextInt();
+            try {
 
-            switch (opcao) {
+                opcao = sc.nextInt();
 
-                case 1:
+                switch (opcao) {
 
-                    System.out.print("\nDigite o nome do cliente: ");
-                    nome = sc.next();
+                    case 1:
 
-                    System.out.print("Digite o numero da conta: ");
-                    conta = sc.next();
+                        System.out.print("\nDigite o nome do cliente: ");
+                        nome = sc.next();
 
-                    System.out.print("Digite a agência: ");
-                    agencia = sc.nextInt();
+                        System.out.print("Digite o numero da conta: ");
+                        conta = sc.next();
 
+                        System.out.print("Digite a agência: ");
+                        agencia = sc.nextInt();
 
-                    clientes.add(new Clientes(nome, conta, agencia));
+                        clientes.add(new Clientes(nome, conta, agencia));
 
-                    System.out.println("Cliente Cadastrado com sucesso!: ");
-                    System.out.println(clientes.get(clientes.size() - 1));
-
-                    break;
-
-                case 2:
-                    if (clientes.size() > 0) {
-                        System.out.println("\nLista de Clientes:");
-                        clientes.forEach(cli -> System.out.print(cli.toString()));
-                        System.out.println();
+                        JOptionPane.showMessageDialog(null, "Cliente Cadastrado com sucesso!\n" + clientes.get(clientes.size() - 1), "Cadastro", JOptionPane.INFORMATION_MESSAGE);
                         break;
-                    }
-                    System.out.println("\nNenhum Cliente Cadastrado\n");
-                    break;
 
-                case 3:
-
-                    if (clientes.size() > 0) {
-
-                        try {
-                            FileWriter arquivo = new FileWriter(caminhoArquivo);
-                            PrintWriter gravarArquivo = new PrintWriter(arquivo);
-                            for (Clientes cliente : clientes) {
-                                gravarArquivo.printf(cliente.toString());
-                            }
-                            arquivo.close();
-                            System.out.print("\nArquivo gravado com sucesso!\n\n");
-                        } catch (Exception e) {
-                            System.out.println("Erro ao gravar o arquivo" + e.getMessage());
+                    case 2:
+                        if (clientes.size() > 0) {
+                            System.out.println("\nLista de Clientes:");
+                            clientes.forEach(cli -> System.out.print(cli.toString()));
+                            System.out.println();
+                            JOptionPane.showMessageDialog(null, "Lista de Clientes:\n" + clientes.listIterator().next(), "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+                            break;
                         }
-                    } else {
-                        System.out.println("\nLista de clientes vazia\n");
-                    }
-                    break;
+                        JOptionPane.showMessageDialog(null, "Nenhum Cliente Cadastrado", "Lista Vazia", JOptionPane.INFORMATION_MESSAGE);
+                        break;
 
-                case 4:
+                    case 3:
+                        if (clientes.size() > 0) {
+                            try {
+                                FileWriter arquivo = new FileWriter(caminhoArquivo);
+                                PrintWriter gravarArquivo = new PrintWriter(arquivo);
+                                for (Clientes cliente : clientes) {
+                                    gravarArquivo.printf(cliente.toString());
+                                }
+                                arquivo.close();
+                                JOptionPane.showMessageDialog(null, "Arquivo gravado com sucesso!", "Arquivo", JOptionPane.INFORMATION_MESSAGE);
 
-                    if (arq.exists()) {
-
-                        StringBuilder titulo = new StringBuilder("             LISTA DE CLIENTES: \n\n");
-
-                        try {
-                            FileReader reader = new FileReader(caminhoArquivo);
-                            BufferedReader lerArquivo = new BufferedReader(reader);
-                            while (true) {
-                                String linha = lerArquivo.readLine();
-                                if (linha == null) break;
-                                titulo.append(linha).append("\n");
+                            } catch (Exception e) {
+                                JOptionPane.showMessageDialog(null, "Erro ao gravar o arquivo" + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
                             }
-                        } catch (Exception e) {
-                            System.out.println("Erro ao Abrir o Arquivo" + e.getMessage());
-                            e.printStackTrace();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Lista de clientes vazia", "Lista Vazia", JOptionPane.INFORMATION_MESSAGE);
                         }
-                        JOptionPane.showMessageDialog(null, titulo.toString(), arq.getName(), JOptionPane.INFORMATION_MESSAGE);
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Arquivo não encontrado", "Erro", JOptionPane.ERROR_MESSAGE);
-                    }
-                    break;
+                        break;
 
-                case 5:
-                    menu = false;
-                    break;
+                    case 4:
+                        if (arq.exists()) {
+                            StringBuilder titulo = new StringBuilder("             ARQUIVO DE CLIENTES: \n\n");
+                            try {
+                                FileReader reader = new FileReader(caminhoArquivo);
+                                BufferedReader lerArquivo = new BufferedReader(reader);
+                                while (true) {
+                                    String linha = lerArquivo.readLine();
+                                    if (linha == null) break;
+                                    titulo.append(linha).append("\n");
+                                }
+                            } catch (Exception e) {
+                                JOptionPane.showMessageDialog(null, "Erro ao Abrir o Arquivo" + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+                            }
+                            JOptionPane.showMessageDialog(null, titulo.toString(), arq.getName(), JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Arquivo não encontrado", "Erro", JOptionPane.ERROR_MESSAGE);
+                        }
+                        break;
 
-                default:
-                    System.out.println("\nOpção Inválida!\n");
-                    break;
+                    case 5:
+                        menu = false;
+                        break;
+
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opção Inválida!", "Erro", JOptionPane.INFORMATION_MESSAGE);
+                        sc.nextLine();
+                }
+            } catch (InputMismatchException e) {
+                JOptionPane.showMessageDialog(null, "Caracter não permitido", "Erro", JOptionPane.ERROR_MESSAGE);
+                sc.nextLine();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro" + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+                return;
             }
         }
     }
